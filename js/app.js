a5d2e0bd492817d8984762e06023876f3e4ccbe8
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-08-10 11:41:21 +0300

    chickenburgermenu

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
-const open =document.getElementById('side-menu');
+/*const hamburger = document.getElementById("burger__header");
+const navLinks = document.querySelector(".navLinks");
+hamburger.addEventListener("click", () => {
+  console.log("hello world!");
+  navLinks.classList.toggle("open");
+  hamburger.classList.toggle("toggle");
+});
+navLinks.addEventListener("click", () => {
+  navLinks.classList.remove("open");
+  hamburger.classList.remove("toggle");
+});*/
+
+const hamburger = document.querySelector(".hamburger")
+const menu_header = document.querySelector(".menu_header")
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  menu_header.classList.toggle("active");
+
+})
 
 
