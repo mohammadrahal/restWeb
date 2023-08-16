17603dc660f77138c4561a0dad8d848bb19b49e5
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-08-16 12:04:00 +0300

    5arabna l about

--- js/app.js
+++ js/app.js
@@ -10,3 +10,59 @@ hamburger.addEventListener("click", () => {
 })
 
 
+function toggleAnswer(index) {
+  let buttons = document.querySelectorAll(".question-btn");
+  let arrowIcon = buttons[index].querySelector("i");
+  let answerDiv = buttons[index].nextElementSibling;
+
+  if (answerDiv.style.display === "none") {
+    answerDiv.style.display = "block";
+    arrowIcon.classList.add("fa-chevron-up");
+    arrowIcon.classList.remove("fa-chevron-down");
+  } else {
+    answerDiv.style.display = "none";
+    arrowIcon.classList.add("fa-chevron-down");
+    arrowIcon.classList.remove("fa-chevron-up");
+  }
+}
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+
