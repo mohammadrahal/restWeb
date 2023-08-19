94792bc4b2c957ce84d3434f77dbc67b55970be3
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-08-19 20:06:12 +0300

    map-about us

--- js/app.js
+++ js/app.js
@@ -25,44 +25,15 @@ function toggleAnswer(index) {
     arrowIcon.classList.remove("fa-chevron-up");
   }
 }
-const scrollElements = document.querySelectorAll(".js-scroll");
 
-const elementInView = (el, dividend = 1) => {
-  const elementTop = el.getBoundingClientRect().top;
 
-  return (
-    elementTop <=
-    (window.innerHeight || document.documentElement.clientHeight) / dividend
-  );
-};
+//map
+let map = L.map('map').setView([33.89045843306141, 35.50522064129898], 30);
 
-const elementOutofView = (el) => {
-  const elementTop = el.getBoundingClientRect().top;
 
-  return (
-    elementTop > (window.innerHeight || document.documentElement.clientHeight)
-  );
-};
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
 
-const displayScrollElement = (element) => {
-  element.classList.add("scrolled");
-};
-
-const hideScrollElement = (element) => {
-  element.classList.remove("scrolled");
-};
-
-const handleScrollAnimation = () => {
-  scrollElements.forEach((el) => {
-    if (elementInView(el, 1.25)) {
-      displayScrollElement(el);
-    } else if (elementOutofView(el)) {
-      hideScrollElement(el)
-    }
-  })
-}
-
-window.addEventListener("scroll", () => { 
-  handleScrollAnimation();
-});
-
+let marker = L.marker([33.89045843306141, 35.50522064129898]).addTo(map);
