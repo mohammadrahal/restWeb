1eece012897e26b8a332408465b21b1c649aa212
Mohammad Rahal <rahalmohamad21@gmail.com>
2023-08-22 10:15:03 +0300

    finished contact us

--- js/validate.js
+++ js/validate.js
@@ -2,8 +2,8 @@ const fname = document.getElementById('name');
 const email = document.getElementById('email');
 const text = document.getElementById('text');
 const submit = document.getElementById('submit');
-const errorEl = document.querySelectorAll(".error"); 
-const succ = document.getElementById('succ')
+const errorEl = document.querySelectorAll(".error");
+const succ = document.getElementById('succ');
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 submit.addEventListener('click', () => {
@@ -15,7 +15,7 @@ submit.addEventListener('click', () => {
         errorEl.forEach(error => {
             error.textContent = "Name can't be blank";
         });
-        clear(errorEl, 3000);
+        clearErrorMessage(errorEl, 3000);
         return false;
     }
 
@@ -23,7 +23,7 @@ submit.addEventListener('click', () => {
         errorEl.forEach(error => {
             error.textContent = "Please enter a valid email";
         });
-        clear(errorEl, 3000);
+        clearErrorMessage(errorEl, 3000);
         return false;
     } 
     
@@ -31,18 +31,24 @@ submit.addEventListener('click', () => {
         errorEl.forEach(error => {
             error.textContent = "Message can't be empty";
         });
-        clear(errorEl, 3000);
+        clearErrorMessage(errorEl, 3000);
         return false;
-    }else {
-        succ.textContent='message send successfully';
-    } 
-});clear(succ, 3000);
+    } else {
+        succ.textContent = 'Message sent successfully';
+        clearSuccessMessage(succ, 3000);
+    }
+});
 
-function clear(errors, timeout) {
+function clearErrorMessage(errors, timeout) {
     setTimeout(() => {
         errors.forEach(error => {
             error.textContent = "";
-           
         });
+    }, timeout);
+}
+
+function clearSuccessMessage(element, timeout) {
+    setTimeout(() => {
+        element.textContent = "";
     }, timeout);
 }
